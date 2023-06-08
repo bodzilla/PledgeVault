b4bcf8107c945be64a9cb7b2ceb033983c312594
Bodrul Choudhury <Bodrul.Choudhury@StoneX.com>
2023-06-08 23:30:31 +0100

    Code clean.

--- PledgeVault.Core/Exceptions/NotFoundException.cs
+++ PledgeVault.Core/Exceptions/NotFoundException.cs
@@ -4,7 +4,7 @@ namespace PledgeVault.Core.Exceptions;
 
 public sealed class NotFoundException : PledgeVaultException
 {
-    public NotFoundException([Optional] string message) : base(message)
+    public NotFoundException([Optional] string message) : base(message ?? "The resource was not found.")
     {
     }
 }
