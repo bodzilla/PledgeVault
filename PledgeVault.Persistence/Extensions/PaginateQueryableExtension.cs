b2fb5eb1b2e2e2d191fa584d1e06be1f27e0de24
Bodrul Choudhury <Bodrul.Choudhury@StoneX.com>
2023-06-11 16:41:00 +0100

    Clean code.

--- PledgeVault.Persistence/Extensions/PaginateQueryableExtension.cs
+++ PledgeVault.Persistence/Extensions/PaginateQueryableExtension.cs
@@ -1,4 +1,5 @@
-﻿using PledgeVault.Core.Dtos.Pagination;
+﻿using PledgeVault.Core.Contracts.Entities;
+using PledgeVault.Core.Dtos.Pagination;
 using System.Linq;
 
 namespace PledgeVault.Persistence.Extensions;
@@ -9,6 +10,6 @@ public static class PaginateQueryableExtension
     /// Given a <see cref="PageOptions"/>, applies pagination to the queryable. Ensure that this method is always called after
     /// any filtering (including <see cref="OnlyActiveEntitiesQueryableExtension"/>) is applied to the queryable, otherwise the pagination will be applied to all entities.
     /// </summary>
-    public static IQueryable<T> WithPagination<T>(this IQueryable<T> queryable, PageOptions pageOptions)
+    public static IQueryable<T> WithPagination<T>(this IQueryable<T> queryable, PageOptions pageOptions) where T : IEntity
         => queryable.Skip((pageOptions.PageNumber - 1) * pageOptions.PageSize).Take(pageOptions.PageSize);
 }
