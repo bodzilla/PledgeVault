697a9d65e2219c6a9fe973a5b795ecbef4a9c808
Bodrul Choudhury <Bodrul.Choudhury@StoneX.com>
2023-06-11 22:03:57 +0100

    Added comments.

--- PledgeVault.Core/Contracts/Entities/Validators/IEntityValidator.cs
+++ PledgeVault.Core/Contracts/Entities/Validators/IEntityValidator.cs
@@ -11,6 +11,12 @@ namespace PledgeVault.Core.Contracts.Entities.Validators;
 /// </summary>
 public interface IEntityValidator<in T> where T : IEntity
 {
+    /// <summary>
+    /// Runs all the validation rules for the entity.
+    /// </summary>
+    /// <param name="type">Will skip certain validation checks based on this.</param>
+    /// <param name="entity">The entity to be validated.</param>
+    /// <param name="cancellationToken">The cancellation token for this request.</param>
     Task ValidateAllRules(EntityValidatorType type, T entity, CancellationToken cancellationToken);
 
     /// <summary>
